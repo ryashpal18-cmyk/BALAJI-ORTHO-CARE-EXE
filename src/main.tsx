d00188612c39d69a36edf33a9c53519232414646
ryashpal18-cmyk <ryashpal18@gmail.com>
2026-06-19 18:07:00 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { startAutoSync } from "./lib/offlineSync";
+import { startAutoBackupScheduler } from "./lib/backup";
+
+startAutoSync();
+startAutoBackupScheduler();
 
 createRoot(document.getElementById("root")!).render(<App />);
